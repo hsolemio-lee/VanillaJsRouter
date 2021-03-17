ca2140c6cbbd0ebe4a454045b08f381a6dc38c7d
hsolemio-lee <hslyjy@gmail.com>
2021-03-17 18:48:53 +0900

    Create ballCanvas.js

---
+++ js/canvas1/ballCanvas.js
@@ -0,0 +1,36 @@
+import {
+    Ball
+} from './ball.js';
+
+export default class BallCanvas {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+        document.getElementById('root').appendChild(this.canvas);
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        this.ball = new Ball(this.stageWidth, this.stageHeight, 60, 5);
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * 2;
+        this.canvas.height = this.stageHeight * 2;
+        this.ctx.scale(2,2);
+    }
+
+    animate(t) {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0,0,this.stageWidth,this.stageHeight);
+
+        this.ball.draw(this.ctx, this.stageWidth, this.stageHeight);
+    }
+}
